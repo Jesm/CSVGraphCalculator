069f88bfd5eae5a978f229e1deaff65da2948e2c
Jonathan Szablevski <jszablevski@hotmail.com>
2015-05-15 23:14:53 -0300

    Exibir CSV a partir do argumento

--- CSVGraphCalculator.go
+++ CSVGraphCalculator.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	// "io"
+	"encoding/csv"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	arquivo, erro:=os.Open(os.Args[1])
+	if erro!=nil {
+		fmt.Printf("%v \n", erro)
+		return
+	}
+
+	leitor:=csv.NewReader(arquivo)
+	valores, erro:=leitor.ReadAll()
+	
+	for x:=0; x<len(valores); x++ {
+		for y:=0; y<len(valores[x]); y++ {
+			fmt.Printf(" %v ", valores[x][y])
+		}
+		fmt.Printf("\n")
+	}
 }
